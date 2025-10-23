59fa223494bb65ce873631bb5a1a12146bc19563
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2025-10-23 15:00:23 +0100

    Firs commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/App.css'
+import AppRoutes from "./routes/Routes";
+
+
+function App() {
+  return <AppRoutes />;
+}
+
+export default App
